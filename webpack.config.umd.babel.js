7619f87b1606f57b229a0b86ac655494aa1373e0
fritz-c <fritz-c@users.noreply.github.com>
2016-08-08 16:39:33 +0900

    Allow access to the NODE_ENV for development-only code

--- webpack.config.umd.babel.js
+++ webpack.config.umd.babel.js
@@ -29,6 +29,9 @@ module.exports = {
     devtool: 'source-map',
     plugins: [
         new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.EnvironmentPlugin([
+            "NODE_ENV",
+        ]),
         new webpack.optimize.UglifyJsPlugin({
             compress: {
                 warnings: false
